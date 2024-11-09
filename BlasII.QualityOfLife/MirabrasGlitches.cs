1d7e8e797b04d43fd164c7540c58869e2b3bacf6
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-08 21:43:22 -0500

    mg: clean up

--- BlasII.QualityOfLife/MirabrasGlitches.cs
+++ BlasII.QualityOfLife/MirabrasGlitches.cs
@@ -1,70 +1,28 @@
-﻿using BlasII.ModdingAPI;
-using BlasII.ModdingAPI.Assets;
+﻿using BlasII.ModdingAPI.Assets;
 using BlasII.ModdingAPI.Input;
-using HarmonyLib;
 using Il2CppLightbug.Kinematic2D.Implementation;
 using Il2CppTGK.Game;
-using Il2CppTGK.Game.Managers;
 
 namespace BlasII.QualityOfLife;
 
 internal class MirabrasGlitches
 {
-    private bool next = false;
-    private int frames = 255;
-
     public void Update()
     {
         if (!Main.QualityOfLife.CurrentSettings.AllowMirabrasGlitches)
             return;
 
-        if (frames > 0 && frames != 255)
-        {
-            frames--;
+        if (!Main.QualityOfLife.InputHandler.GetButtonDown(ButtonType.NextWeapon))
+            return;
 
-            var changeWeapon = AssetStorage.Abilities[ABILITY_IDS.ChangeWeapon];
+        if (CoreCache.EquipmentManager.CountUnlockedWeapons() < 2)
+            return;
 
-            var controller = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterController2D>();
-            controller.ActivateAbilityByType(changeWeapon);
-        }
-        //ModLog.Warn("Frames: " + frames);
+        var changeWeapon = AssetStorage.Abilities[ABILITY_IDS.ChangeWeapon];
+        var fullPrayer = AssetStorage.Abilities[ABILITY_IDS.FullPrayer];
 
-        //if (frames == 0)
-        //{
-        //    next = false;
-        //    frames = 255;
-        //    var changeWeapon = AssetStorage.Abilities[ABILITY_IDS.ChangeWeapon];
-        //    var fullPrayer = AssetStorage.Abilities[ABILITY_IDS.FullPrayer];
-
-        //    var controller = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterController2D>();
-        //    controller.CancelAbility(fullPrayer);
-        //    controller.ActivateAbilityByType(changeWeapon);
-        //}
-
-        if (Main.QualityOfLife.InputHandler.GetButtonDown(ButtonType.NextWeapon))
-        {
-            var changeWeapon = AssetStorage.Abilities[ABILITY_IDS.ChangeWeapon];
-            var fullPrayer = AssetStorage.Abilities[ABILITY_IDS.FullPrayer];
-
-            ModLog.Info("Unlocked: " + (CoreCache.EquipmentManager.CountUnlockedWeapons() > 1));
-            if (CoreCache.EquipmentManager.CountUnlockedWeapons() < 2)
-                return;
-
-            var controller = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterController2D>();
-            controller.CancelAbility(fullPrayer);
-            controller.ActivateAbilityByType(changeWeapon);
-
-            next = true;
-            frames = 10;
-        }
-    }
-}
-
-[HarmonyPatch(typeof(AbilityLockManager), nameof(AbilityLockManager.SetAbility))]
-class t
-{
-    public static void Postfix(IAbilityTypeRef abilityID, bool status)
-    {
-        ModLog.Error($"Setting {abilityID.name} to {status}");
+        var controller = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterController2D>();
+        controller.CancelAbility(fullPrayer);
+        controller.ActivateAbilityByType(changeWeapon);
     }
 }
