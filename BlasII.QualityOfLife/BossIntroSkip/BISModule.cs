2bc3ac32747987eafbd2a105a00dd41c54ae559b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-09 13:32:51 -0500

    skip: register all skips as modules

---
+++ BlasII.QualityOfLife/BossIntroSkip/BISModule.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BlasII.QualityOfLife.BossIntroSkip;
+
+internal class BISModule : BaseModule
+{
+    public override string Name { get; } = "BossIntroSkip";
+    public override KeyCode DefaultKey { get; } = KeyCode.Keypad3;
+}
