9d477bafe6c9f62460160a798e19a7978d255c56
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-09 10:27:27 -0500

    ct: change to object cache

--- BlasII.QualityOfLife/ConsistentTyphoon/CTModule.cs
+++ BlasII.QualityOfLife/ConsistentTyphoon/CTModule.cs
@@ -1,4 +1,4 @@
-﻿using BlasII.ModdingAPI;
+﻿using BlasII.ModdingAPI.Utils;
 using Il2CppLightbug.Kinematic2D.Core;
 using Il2CppTGK.Game;
 using UnityEngine;
@@ -9,43 +9,31 @@ internal class CTModule : BaseModule
 {
     private Vector3 _lastPosition = Vector3.zero;
 
+    private ObjectCache<CharacterBody2DImpl> _playerBody;
+    private ObjectCache<Animator> _playerAnimator;
+
+    public override void OnStart()
+    {
+        // Requires full delegate syntax otherwise it crashes
+        _playerBody = new ObjectCache<CharacterBody2DImpl>(() => CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterBody2DImpl>());
+        _playerAnimator = new ObjectCache<Animator>(() => CoreCache.PlayerSpawn.PlayerInstance.GetComponentInChildren<Animator>());
+    }
+
     public override void OnUpdate()
     {
         if (!Main.QualityOfLife.CurrentSettings.ConsistentTyphoon || CoreCache.PlayerSpawn.PlayerInstance == null)
             return;
 
-        int currentAnimation = PlayerAnim.GetCurrentAnimatorStateInfo(0).nameHash;
+        int currentAnimation = _playerAnimator.Value.GetCurrentAnimatorStateInfo(0).nameHash;
 
         // If in censer spin animation, override movement with typhoon force
         if (currentAnimation == TYPHOON_ANIM_HASH)
         {
             Vector3 newPos = _lastPosition + Vector3.up * TYPHOON_FORCE * Time.deltaTime;
-            PlayerBody.bodyTransform = new BodyTransform() { position = newPos };
+            _playerBody.Value.bodyTransform = new BodyTransform() { position = newPos };
         }
 
-        _lastPosition = PlayerBody.bodyTransform.position;
-    }
-
-    private CharacterBody2DImpl _playerBody;
-    private CharacterBody2DImpl PlayerBody
-    {
-        get
-        {
-            if (_playerBody == null)
-                _playerBody = CoreCache.PlayerSpawn.PlayerInstance.GetComponent<CharacterBody2DImpl>();
-            return _playerBody;
-        }
-    }
-
-    private Animator _playerAnim;
-    private Animator PlayerAnim
-    {
-        get
-        {
-            if (_playerAnim == null)
-                _playerAnim = CoreCache.PlayerSpawn.PlayerInstance.GetComponentInChildren<Animator>();
-            return _playerAnim;
-        }
+        _lastPosition = _playerBody.Value.bodyTransform.position;
     }
 
     private const float TYPHOON_FORCE = 3.4f;
