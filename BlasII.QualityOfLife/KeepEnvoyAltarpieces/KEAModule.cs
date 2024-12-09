a4ac9ac9bebd4d3b8267f882897ff59ec86fab04
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-09 13:48:09 -0500

    envoy: add base module

---
+++ BlasII.QualityOfLife/KeepEnvoyAltarpieces/KEAModule.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BlasII.QualityOfLife.KeepEnvoyAltarpieces;
+
+internal class KEAModule : BaseModule
+{
+    public override string Name { get; } = "KeepEnvoyAltarpieces";
+    public override KeyCode DefaultKey { get; } = KeyCode.Keypad5;
+}
