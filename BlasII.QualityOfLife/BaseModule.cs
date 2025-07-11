879562cec8d2aaee1aa0971a9e53c9e17253dbab
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-11 09:38:54 -0400

    core: add type splitting

--- BlasII.QualityOfLife/BaseModule.cs
+++ BlasII.QualityOfLife/BaseModule.cs
@@ -1,10 +1,25 @@
-﻿
+﻿using BlasII.ModdingAPI;
+
 namespace BlasII.QualityOfLife;
 
 internal abstract class BaseModule
 {
-    public abstract string Name { get; }
-    public abstract int Order { get; }
+    public string Name { get; }
+    public int Order { get; }
+
+    public BaseModule()
+    {
+        ModLog.Info(this.GetType().Name);
+
+        string type = GetType().Name;
+        int sep = type.IndexOf('_');
+
+        Name = type[..sep];
+        Order = int.Parse(type[(sep + 1)..]);
+
+        ModLog.Warn(Name);
+        ModLog.Warn(Order);
+    }
 
     public virtual void OnStart() { }
 
