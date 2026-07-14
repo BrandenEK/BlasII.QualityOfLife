d9325f20eaa3d3793e983b933b37b0c1921ed83d
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-13 20:19:59 -0400

    cs: try to fix fade issue

--- BlasII.QualityOfLife/Modules/CutsceneSkip.cs
+++ BlasII.QualityOfLife/Modules/CutsceneSkip.cs
@@ -27,10 +27,16 @@ class PlayCutscene_PlayAndWait_Patch_CS
         if (BANNED_CUTSCENES.Contains(name))
             return true;
 
-        if (FADE_CUTSCENES.Contains(name))
-            FadeWindowLogic_FadeAsync_Patch_CS.FADE_FLAG = true;
+        //if (FADE_CUTSCENES.Contains(name))
+        //{
+        //    __instance.fadeOut1Color.Value = Color.black;
+        //    __instance.fadeOut2Color.Value = Color.black;
+        //}
+            //FadeWindowLogic_FadeAsync_Patch_CS.FADE_FLAG = true;
 
         ModLog.Warn("Skipping cutscene: " + name);
+        ModLog.Error("Fade: " + __instance.waitForFinalFade.Value);
+        //__instance.waitForFinalFade.Value = false;
         __instance.Finish();
         return false;
     }
@@ -38,6 +44,16 @@ class PlayCutscene_PlayAndWait_Patch_CS
     private static readonly string[] BANNED_CUTSCENES = ["CTS17_id"];
     private static readonly string[] FADE_CUTSCENES = ["CTS08_id", "CTS10_id", "CTS12_id"];
 }
+
+//[HarmonyPatch(typeof(FadeWindowLogic), nameof(FadeWindowLogic.ClearFadeAsync))]
+//class y
+//{
+//    public static void Postfix(FadeWindowLogic __instance)
+//    {
+//        ModLog.Warn("Closing fade");
+//        __instance.colorImage?.color = new Color32(0, 0, 0, 0);
+//    }
+//}
 
 /// <summary>
 /// Skip quotes from playmaker
@@ -59,18 +75,39 @@ class ShowQuote_OnEnter_Patch_CS
 /// <summary>
 /// Force the fadeout to be black after skipping certain cutscenes
 /// </summary>
-[HarmonyPatch(typeof(FadeWindowLogic), nameof(FadeWindowLogic.FadeAsync), typeof(float), typeof(Action), typeof(Color))]
+[HarmonyPatch(typeof(FadeWindowLogic), nameof(FadeWindowLogic.FadeToColorAsync))]
 class FadeWindowLogic_FadeAsync_Patch_CS
 {
-    public static void Prefix(FadeWindowLogic __instance, ref Color targetColor)
+    public static void Prefix(FadeWindowLogic __instance, ref Color color)
     {
-        if (!FADE_FLAG)
-            return;
+        //if (!FADE_FLAG)
+        //    return;
 
-        ModLog.Info("Forcing fade to black");
-        targetColor = Color.black;
-        FADE_FLAG = false;
+        //ModLog.Info("Forcing fade to black");
+        //color = Color.black;
+        //FADE_FLAG = false;
     }
 
     public static bool FADE_FLAG { get; set; }
 }
+
+[HarmonyPatch(typeof(FadeWindowLogic), nameof(FadeWindowLogic.FadeFromCurrentAsync))]
+class ty
+{
+    public static void Prefix(FadeWindowLogic __instance)
+    {
+        //__instance.colorImage.color = Color.black;
+    }
+}
+
+/// <summary>
+/// Always replace fade current color with black - prevents fade being locked to white after boss defeat
+/// </summary>
+//[HarmonyPatch(typeof(FadeWindowLogic), nameof(FadeWindowLogic.FadeToCurrentColorAsync))]
+//class FadeWindowLogic_FadeToCurrentColorAsync_Patch
+//{
+//    public static void Prefix(FadeWindowLogic __instance)
+//    {
+//        __instance.colorImage.color = new Color(0, 0, 0, __instance.colorImage.color.a);
+//    }
+//}
