a9ddea7aeb16789cd3647a3853082556db97ac6f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-07-23 09:11:05 -0400

    qsa: test input for swapping weapons

--- BlasII.QualityOfLife/Modules/QuickSwapAlts.cs
+++ BlasII.QualityOfLife/Modules/QuickSwapAlts.cs
@@ -1,9 +1,10 @@
 ﻿using BlasII.ModdingAPI;
-using System;
+using BlasII.ModdingAPI.Assets;
+using Il2CppTGK.Game;
+using Il2CppTGK.Game.Components.Abilities;
+using Il2CppTGK.Game.Components.Attack.Data;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
 namespace BlasII.QualityOfLife.Modules;
 
@@ -12,6 +13,17 @@ internal class QuickSwapAlts_9 : BaseModule
     public override void OnUpdate()
     {
         if (Main.QualityOfLife.InputHandler.GetButton(ModdingAPI.Input.ButtonType.Interact))
-            ModLog.Error("Hold interact");
+        {
+            //ModLog.Error("Hold interact");
+
+            if (UnityEngine.Input.GetKeyDown(KeyCode.R))
+            //if (Main.QualityOfLife.InputHandler.GetButtonDown(ModdingAPI.Input.ButtonType.ChangeWeapon))
+            {
+                ModLog.Warn("quick swap");
+                Object.FindObjectOfType<ChangeWeaponAbility>().ChangeWeapon(AssetStorage.Weapons[WEAPON_IDS.Whip]);
+                CoreCache.EquipmentManager.SwapWeapons(AssetStorage.Weapons[WEAPON_IDS.Censer], AssetStorage.Weapons[WEAPON_IDS.Whip]);
+
+            }
+        }
     }
 }
